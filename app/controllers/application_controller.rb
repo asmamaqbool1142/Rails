f33d803aa2609854bfd8acc9cbd8bad68156fd79
asmamaqbool1142 <asmamaqbool24@gmail.com>
2022-04-01 11:05:35 +0500

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
     def hello 
-        render html: "hello, world!"
-    end 
+         render html: "hello, world!"
+    end
+    def mid
+        render html:"It is going to be a good day"
+    end
     def goodbye
         render html:"Goodbye"
     end
